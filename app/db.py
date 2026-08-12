60be9d41a8528e7cc8b6d95687bcfc4cbe7c4b7b
ean-jp <paaekow.ackon@gmail.com>
2026-08-12 10:33:57 +0000

    Fix MySQL application connection

--- app/db.py
+++ app/db.py
@@ -5,7 +5,9 @@ import streamlit as st
 def get_connection():
     return mysql.connector.connect(
         host=st.secrets["mysql"]["host"],
+        port=int(st.secrets["mysql"]["port"]),
         user=st.secrets["mysql"]["user"],
         password=st.secrets["mysql"]["password"],
         database=st.secrets["mysql"]["database"],
+        use_pure=True
     )
